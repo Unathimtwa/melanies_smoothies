a28920b60788c5af6ac42a03c45282ba9e0b4343
Unathi <Mtwaunathi@gmail.com>
2024-08-26 12:39:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
 	ingredients_string = ''
-	for each_fruit in ingredients_list:
-		ingredients_string += each_fruit + ''
-		st.subheader(each_fruit +' Nutrition Information')
+	for fruit_chosen in ingredients_list:
+		ingredients_string += fruit_chosen + ''
+		st.subheader(fruit_chosen +' Nutrition Information')
 
 		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" +fruit_chosen)
 		fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
